09765f81f7741793dab011beaf096ede0547c854
Pokoai <pokeusr@outlook.com>
2022-10-19 01:02:51 +0800

    fix bug: 定时器计数变量2类型由u8改为u16

--- include/timer.h
+++ include/timer.h
@@ -14,8 +14,8 @@
 
 
 static u8 TIMER0_CNT = 0;     // 定时器0计数器1
-static u8 TIMER0_CNT2 = 0;    // 定时器0计数器2
-static u8 TIMER0_CNT3 = 0;    // 定时器0计数器3
+static u16 TIMER0_CNT2 = 0;    // 定时器0计数器2
+// static u8 TIMER0_CNT3 = 0;    // 定时器0计数器3
 
 extern void timer_init();
 
